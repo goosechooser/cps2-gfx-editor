2a629c0f6b88203baab6b6ea98e94dbc597b806f
Marshall <xtremebass2k@gmail.com>
2016-12-09 03:51:56 -0800

    refactored and renamed modules

---
+++ src/file_handler_sample.py
@@ -0,0 +1,13 @@
+from gfx_file_handler import interleave_files, deinterleave_files
+
+if __name__ == "__main__":
+    GFX_DATA = interleave_files("inputs/gfx_original")
+    for k, v in GFX_DATA.items():
+        with open('outputs/refactor/roms/' + k, 'wb') as f:
+            f.write(v)
+
+    GFX_DATA = deinterleave_files("inputs/refactor")
+    for k, v in GFX_DATA.items():
+        with open('outputs/refactor/roms/' + k, 'wb') as f:
+            f.write(v)
+    
--- src/gfx_file_handler_test.py
+++ src/gfx_file_handler_test.py
@@ -1,26 +1,24 @@
-from Cps2GraphicsFileHandler import Cps2GraphicsFileHandler
-
-handler = Cps2GraphicsFileHandler()
+from gfx_file_handler import deinterleave, interleave
 
 def test_deinterleave():
     test_data = bytearray.fromhex('F1 FF 00 00 00 01 FF FF')
     #handler = Cps2GraphicsFileHandler()
 
-    deinterleaved = handler.deinterleave(test_data, 2)
+    deinterleaved = deinterleave(test_data, 2)
     assert bytearray(deinterleaved[0]).hex() == 'f1ff0001'
     assert bytearray(deinterleaved[1]).hex() == '0000ffff'
 
-    deinterleaved = handler.deinterleave(test_data, 4)
+    deinterleaved = deinterleave(test_data, 4)
     assert bytearray(deinterleaved[0]).hex() == 'f1ff0000'
     assert bytearray(deinterleaved[1]).hex() == '0001ffff'
 
 def test_interleave():
     test_data = [bytearray.fromhex('F1FF0001'),
                  bytearray.fromhex('0000FFFF')]
-    interleaved = handler.interleave(test_data[0], test_data[1], 2)
+    interleaved = interleave(test_data[0], test_data[1], 2)
     assert bytearray(interleaved).hex() == 'f1ff00000001ffff'
 
-    interleaved = handler.interleave(test_data[0], test_data[1], 4)
+    interleaved = interleave(test_data[0], test_data[1], 4)
     assert bytearray(interleaved).hex() == 'f1ff00010000ffff'
 
 if __name__ == "__main__":
