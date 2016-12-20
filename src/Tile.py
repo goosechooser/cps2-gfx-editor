679ff368dc982452f7a9526e26ff6bc4f171cd1d
Marshall <xtremebass2k@gmail.com>
2016-12-20 02:56:49 -0800

    Big refactoring ->Moving more methods to be class methods ->Blank Tile constructor ->More things related to the creation of ColorTile

--- src/Tile.py
+++ src/Tile.py
@@ -1,163 +1,195 @@
 from __future__ import generators
-from struct import Struct
+from struct import Struct, iter_unpack
+import numpy as np
 
 class Tile():
 
-    def __init__(self, addr, data, dimensions):
-        self.tile_addr = addr
-        self.tile_data = data
-        self.tile_dimensions = dimensions
+    def __init__(self, addr, data, dimensions=16, packed=True):
+        self._tile_addr = addr
+        if packed:
+            self._tile_data = data
+        else:
+            self._tile_data = self.pack(data)
+        self._tile_dimensions = dimensions
+        self._packed = packed
+
+    def __repr__(self):
+        return "Address: " + str(self._tile_addr) + "\nDimension: " + str(self._tile_dimensions) + "\nData: " + str(self._tile_data) + ")"
 
     @property
     def address(self):
         """Get the address in memory where the tile is located."""
-        return self.tile_addr
+        return self._tile_addr
+
     @address.setter
     def address(self, value):
-        self.tile_addr = value
+        self._tile_addr = value
 
     @property
     def data(self):
         """Get the bitplane packed data."""
-        return self.tile_data
+        return self._tile_data
 
     @property
     def dimensions(self):
         """Get the pixel value data."""
-        return self.tile_dimensions
+        return self._tile_dimensions
+
+    @property
+    def packed(self):
+        """Get the pack state of the tile."""
+        return self._packed
 
 #Functions to manipulate tile objects below ig
 #Doesn't check if a tile is 8x8 or 16x16, should it?
 #Should this return raw data or should it return a Tile obj?
-def unpack_tile(tile):
-    """Unpacks a 4bpp planar tile.
+    def unpack(self):
+        """Unpacks a 4bpp planar tile.
 
-    Returns a byte() of pixel values.
-    """
-    tile_fmt = Struct(32 * 'c')
-    tile_iter = tile_fmt.iter_unpack(tile.data)
+        Returns a byte() of pixel values.
+        """
+        tile_fmt = Struct(32 * 'c')
+        tile_iter = tile_fmt.iter_unpack(self._tile_data)
 
-    tiles = [_bitplanes_to_tile(b''.join(tile)) for tile in tile_iter]
-    return b''.join(tiles)
+        tiles = [self._bitplanes_to_tile(b''.join(tile)) for tile in tile_iter]
+        return b''.join(tiles)
 
-def _bitplanes_to_tile(data):
-    bitplanes = _unpack_bitplanes(data)
+    def _bitplanes_to_tile(self, data):
+        bitplanes = self._unpack_bitplanes(data)
 
-    pixels = []
-    for i in range(0, 8):
-        temp_val = [bitplanes[0][i], bitplanes[1][i],
-                    bitplanes[2][i], bitplanes[3][i]]
-        row = _make_row_of_pixels(temp_val)
+        pixels = []
+        for i in range(0, 8):
+            temp_val = [bitplanes[0][i], bitplanes[1][i],
+                        bitplanes[2][i], bitplanes[3][i]]
+            row = self._make_row_of_pixels(temp_val)
 
-        pixels.append(row)
+            pixels.append(row)
 
-    return b''.join(pixels)
+        return b''.join(pixels)
 
-def _unpack_bitplanes(data):
-    """The values that make up a row of pixels are organized like:
-    [bp1-row1] [bp2-row1] [bp3-row1] [bp4-row1]...
-    [bp1-row8] [bp2-row8] [bp3-row8] [bp4-row8]
+    def _unpack_bitplanes(self, data):
+        """The values that make up a row of pixels are organized like:
+        [bp1-row1] [bp2-row1] [bp3-row1] [bp4-row1]...
+        [bp1-row8] [bp2-row8] [bp3-row8] [bp4-row8]
 
-    Returns a list of lists containing bitplane values (bytes).
-    """
-    planes = [[], [], [], []]
-    data_iter = Struct('c').iter_unpack(data)
+        Returns a list of lists containing bitplane values (bytes).
+        """
+        planes = [[], [], [], []]
+        data_iter = Struct('c').iter_unpack(data)
 
-    for bp in data_iter:
-        planes[0].extend(*bp)
-        planes[1].extend(*next(data_iter))
-        planes[2].extend(*next(data_iter))
-        planes[3].extend(*next(data_iter))
+        for bp in data_iter:
+            planes[0].extend(*bp)
+            planes[1].extend(*next(data_iter))
+            planes[2].extend(*next(data_iter))
+            planes[3].extend(*next(data_iter))
 
-    return planes
+        return planes
 
-def _make_row_of_pixels(bitplane_rows):
-    """Converts the 4 bytes of bitplanes 1-4 for a given row in the tile into a row of pixel values
+    def _make_row_of_pixels(self, bitplane_rows):
+        """Converts the 4 bytes of bitplanes 1-4 for a given row into a row of pixel values
 
-    Returns bytes()
-    """
-    mask = int(b'00000001')
-    bitplane_values = bitplane_rows
-    row_of_pixels = []
-    for loc in range(0, 8):
-        masked = [value & mask for value in bitplane_values]
-        bitplane_values = [value >> 1 for value in bitplane_values]
-        bit_plane_val = masked[0] << 3 | masked[1] << 2 | masked[2] << 1 | masked[3]
-        row_of_pixels.append(bit_plane_val.to_bytes(1, byteorder='big'))
+        Returns bytes()
+        """
+        mask = int(b'00000001')
+        bitplane_values = bitplane_rows
+        row_of_pixels = []
+        for loc in range(0, 8):
+            masked = [value & mask for value in bitplane_values]
+            bitplane_values = [value >> 1 for value in bitplane_values]
+            bit_plane_val = masked[0] << 3 | masked[1] << 2 | masked[2] << 1 | masked[3]
+            row_of_pixels.append(bit_plane_val.to_bytes(1, byteorder='big'))
 
-    row_of_pixels.reverse()
+        row_of_pixels.reverse()
 
-    return b''.join(row_of_pixels)
+        return b''.join(row_of_pixels)
 
-def pack_data(data):
-    """Converts pixel values into 4bpp and packs it for Tile use.
-    Returns bytes()
-    """
+    def pack(self, data):
+        """Converts pixel values into 4bpp and packs it for Tile use.
+        Returns bytes()
+        """
 
-    tile_fmt = Struct(32 * 'c')
-    tile_iter = tile_fmt.iter_unpack(data)
+        tile_fmt = Struct(32 * 'c')
+        tile_iter = tile_fmt.iter_unpack(data)
 
-    bitplane_values = [_tile_to_bitplanes(b''.join(tile)) for tile in tile_iter]
-    return b''.join(bitplane_values)
+        bitplane_values = [self._tile_to_bitplanes(b''.join(tile)) for tile in tile_iter]
+        return b''.join(bitplane_values)
 
-def _tile_to_bitplanes(data):
-    bitplanes = []
-    row_fmt = Struct(8 * 'c')
-    row_iter = row_fmt.iter_unpack(data)
+    def _tile_to_bitplanes(self, data):
+        bitplanes = []
+        row_fmt = Struct(8 * 'c')
+        row_iter = row_fmt.iter_unpack(data)
 
-    bitplanes = [_pixel_row_to_4bpp(row) for row in row_iter]
+        bitplanes = [self._pixel_row_to_4bpp(row) for row in row_iter]
 
-    return b''.join(bitplanes)
+        return b''.join(bitplanes)
 
-def _pixel_row_to_4bpp(row):
-    bitplanes = [0, 0, 0, 0]
-    mask = int(b'00000001')
-    row = [int.from_bytes(val, byteorder='big') for val in row]
+    def _pixel_row_to_4bpp(self, row):
+        bitplanes = [0, 0, 0, 0]
+        mask = int(b'00000001')
+        row = [int.from_bytes(val, byteorder='big') for val in row]
 
-    for pixel in row:
-        for i, plane in enumerate(bitplanes):
-            bitplanes[i] = (plane << 1) | (pixel & mask)
-            pixel = pixel >> 1
+        for pixel in row:
+            for i, plane in enumerate(bitplanes):
+                bitplanes[i] = (plane << 1) | (pixel & mask)
+                pixel = pixel >> 1
 
-    bitplanes.reverse()
-    return bytearray(bitplanes)
+        bitplanes.reverse()
+        return bytearray(bitplanes)
+
+    def to_array(self):
+        """Uses 8x8 or 16x16 Tile data to create an array of the tile's data.
+
+        Returns an array.
+        """
+        interleaved_tile = self.interleave_subtiles()
+
+        tile_fmt = interleaved_tile.dimensions * 'c'
+        tile_iter = iter_unpack(tile_fmt, interleaved_tile.unpack())
+        tiles = [subtile for subtile in tile_iter]
+
+        return np.array(tiles)
 
 #Would need to interleave like [subtile1-row1] [subtile3-row1]
 #                          ... [subtile1-row16] [subtile3-row16]
 #                              [subtile2-row1] [subtile4-row1]
 #                          ... [subtile4-row16] [subtile4-row16]
-def interleave_subtiles(unpacked_tile_data):
-    """Row interleaves the 4 8x8 subtiles in a 16x16 tile.
+    def interleave_subtiles(self):
+        """Row interleaves the 4 8x8 subtiles in a 16x16 tile.
 
-    Returns bytes().
-    """
-    tile_fmt = Struct(64 * 'c')
-    tile_iter = tile_fmt.iter_unpack(unpacked_tile_data)
+        Returns bytes().
+        """
+        tile_fmt = Struct(32 * 'c')
+        tile_iter = tile_fmt.iter_unpack(self._tile_data)
 
-    subtiles = [b''.join(subtile) for subtile in tile_iter]
+        subtiles = [b''.join(subtile) for subtile in tile_iter]
 
-    top = _interleave(subtiles[0], subtiles[2])
-    bottom = _interleave(subtiles[1], subtiles[3])
+        top = self._interleave(subtiles[0], subtiles[2])
+        bottom = self._interleave(subtiles[1], subtiles[3])
 
-    interleaved = [*top, *bottom]
-    return b''.join(interleaved)
+        interleaved = [*top, *bottom]
+        return Tile(self._tile_addr, b''.join(interleaved), self._tile_dimensions)
 
-def _interleave(subtile1, subtile2):
-    """Interleaves two 8x8 tiles like
-    [subtile1-row1] [subtile2-row1] ...
-    [subtile1-row16] [subtile2-row16]
+    def _interleave(self, subtile1, subtile2):
+        """Interleaves two 8x8 tiles like
+        [subtile1-row1] [subtile2-row1] ...
+        [subtile1-row16] [subtile2-row16]
 
-    Returns bytes()
-    """
-    interleaved = []
-    interleave_fmt = Struct(8 * 'c')
+        Returns bytes()
+        """
+        interleaved = []
+        interleave_fmt = Struct(4 * 'c')
 
-    left_iter = interleave_fmt.iter_unpack(subtile1)
-    right_iter = interleave_fmt.iter_unpack(subtile2)
+        left_iter = interleave_fmt.iter_unpack(subtile1)
+        right_iter = interleave_fmt.iter_unpack(subtile2)
 
-    for i in left_iter:
-        right_next = next(right_iter)
-        interleaved.extend([*i, *right_next])
+        for i in left_iter:
+            right_next = next(right_iter)
+            interleaved.extend([*i, *right_next])
 
-    return interleaved
+        return interleaved
+
+class EmptyTile(Tile):
+
+    def __init__(self, dimensions):
+        super().__init__('BLANK', None, dimensions)
 
