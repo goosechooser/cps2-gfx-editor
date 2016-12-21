f070bd95b2422b67ba870f4ebe6def1118b95bda
Marshall <xtremebass2k@gmail.com>
2016-12-21 02:45:52 -0800

    Moved functionality to Sprite.Factory

--- samples/put_benwas_face_into_video_games.py
+++ samples/put_benwas_face_into_video_games.py
@@ -1,38 +1,6 @@
 #from struct import iter_unpack
 from PIL import Image
-from src import ColorTile, tile_printer
-
-def argb_to_rgb(color):
-    return bytes.fromhex(color[1] * 2 + color[2] * 2 + color[3] * 2)
-    #return color[1] * 2 + color[2] * 2 + color[3] * 2
-
-def format_line_read(line):
-    strip_line = line.lstrip('{')
-    strip_line = strip_line.rstrip(' }\n')
-    properties = strip_line.split(", ")
-    line_properties = dict()
-    for prop in properties:
-        stripped = prop.split(" = ")
-        line_properties[stripped[0].lstrip(' ')] = stripped[1]
-
-    del line_properties['pal_number']
-    return line_properties
-
-def read_tile_file(file_name):
-    tile_dicts = []
-
-    with open(file_name, 'r') as f:
-        for line in f:
-            formatted = format_line_read(line)
-            tile_dicts.append(formatted)
-
-    for td in tile_dicts:
-        tile_colors = []
-        for color in td['palette'].split(" "):
-            tile_colors.append(argb_to_rgb(color))
-        td['palette'] = tile_colors
-
-    return tile_dicts
+from src import tile_printer, Sprite, Tile
 
 def list_to_2D_list(list_1d, row_length):
     list_2d = []
@@ -42,47 +10,47 @@ def list_to_2D_list(list_1d, row_length):
 
     return list_2d
 
-def make_color_tiles(gfx_file, tile_numbers):
-    dmitri_tiles = tile_printer.make_tiles(gfx_file, tile_numbers)
-    dmitri_array = tile_printer.process_tile_order(dmitri_tiles)
-    dmitri_array = tile_printer.concat_arrays(dmitri_array)
+def print_color_picture(tiles, picture_name):
+    color_array = tile_printer.process_tile_order(tiles)
+    concat_array = tile_printer.concat_arrays(color_array)
 
-    image = Image.fromarray(dmitri_array, 'RGB')
-    image.save("bless.bmp")
-
-def invert_value(value):
-    return bytes(abs(value - 16))
+    image = Image.fromarray(concat_array, 'RGB')
+    image.save(picture_name + ".png")
 
 def do_it():
     gfx_file = "inputs/refactor/vm3.13.15.17.19.final"
-    tile_dicts = read_tile_file("inputs/tile_info_new.txt")
-    tile_numbers = [td['tile_number'] for td in tile_dicts]
+    tile_file = "inputs/tile_info.txt"
 
+    sprites = []
+    tile_factory = Tile.Factory(gfx_file)
+    tile_factory.open()
 
-    tiles = tile_printer.make_tiles(gfx_file, tile_numbers)
+    sprite_factory = Sprite.Factory(tile_file, tile_factory)
+    sprite_factory.open()
 
-    #is this the new make_color_tiles()?
-    color_tiles = []
-    for i, tile in enumerate(tiles):
-        t_d = tile_dicts[i]
-        palette = t_d['palette']
-        location = (int(t_d['x']), int(t_d['y']))
-        size = (int(t_d['width']), int(t_d['height']))
-        color_tile = ColorTile.ColorTile(tile.address, tile.data, palette, location, size)
-        color_tiles.append(color_tile)
+    sprite = sprite_factory.new()
+    while sprite is not 'EOF':
+        sprites.append(sprite)
+        sprite = sprite_factory.new()
 
-    sorted_tiles = sorted(color_tiles,
-                          key=lambda tile: (tile.location[0], tile.location[1]))
+    for sprite in sprites:
+        print(sprite)
 
-    sorted_tiles[0].tobmp("help")
-    sorted_tiles = list_to_2D_list(sorted_tiles[:25], 5)
+    print(len(sprites))
+    #print(sprite.to2dlist())
+    sprite_factory.close()
+    tile_factory.close()
+    #sprite_factory = Sprite.Factory(tile_file, Tile.Factory(gfx_file))
 
-    
-    dmitri_array = tile_printer.process_tile_order(sorted_tiles)
-    dmitri_array = tile_printer.concat_arrays(dmitri_array)
+    #make_tile_group(gfx_file, tile_file)
+    #color_tiles = make_color_tiles(gfx_file, tile_file)
 
-    image = Image.fromarray(dmitri_array, 'RGB')
-    image.save("bless.bmp")
+    #sorted_tiles = sorted(color_tiles,
+    #                      key=lambda tile: (tile.location[0], tile.location[1]))
+
+    #print(len(sorted_tiles))
+    #sorted_2d = list_to_2D_list(sorted_tiles[:70], 7)
+    #print_color_picture(sorted_2d, "bless")
 
 def put_benwa_into_video_game(benwa_path, gfx_file):
     benwaddrs = [['1B4E0', '1B4E1', '1B4E2'],
