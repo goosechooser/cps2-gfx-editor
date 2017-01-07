7af6390e4b7721951e22adc8dd2a1e5fd366ff2b
Marshall <xtremebass2k@gmail.com>
2017-01-07 06:20:10 -0800

    Cleaned up. Pull data from mame's memory

--- samples/live_mame_processing.py
+++ samples/live_mame_processing.py
@@ -6,13 +6,18 @@ from threading import Thread
 import json
 import jsonpickle
 from src import Sprite
+from src import helper
 
+#Sequential (read: original) version of 'concurrent_mame_processing' 
 #This launches MAME in a subprocess and pipes back the raw data for processing
 #Lets MAME run at like 96% speed instead of 10% or w/e it was doing before
 
-#Look into setting up logger u shit!!
+#Change these as needed
 GFX_FILE = "inputs/refactor/vm3.13.15.17.19.final"
 TILE_FILE = "inputs/op_info_json.txt"
+MAME_PATH = "D:\\Desktop\\mame174\\"
+LUA_SCRIPT = "lua_scripts\\scrape_data_concurrent.lua"
+SPRITE_OUTPUT_FOLDER = "\\inputs\\sample_json\\"
 
 jsonpickle.set_encoder_options('simplejson', ident=4)
 BASE_PATH = os.getcwd()
@@ -24,22 +29,6 @@ def enqueue_output(out):
         Q.put(line)
 
     out.close()
-
-def mask_sprite_data(sprite_data):
-    byte_data = [int(data, 16) for data in sprite_data]
-
-    dict_ = {}
-    dict_['x'] = byte_data[0] & 0x03FF
-    dict_['y'] = byte_data[1] & 0x03FF
-    dict_['height'] = ((byte_data[3] & 0xF000) >> 12) + 1
-    dict_['width'] = ((byte_data[3] & 0x0F00) >> 8) + 1
-    #hex: {0:x};  oct: {0:o};  bin: {0:b}".format(42)
-    top_half = "{0:#x}".format((byte_data[1] & 0x6000) >> 13)
-    bottom_half = "{0:x}".format(byte_data[2])
-    dict_['tile_number'] = top_half + bottom_half
-    dict_['pal_number'] = "{0:x}".format(byte_data[3] & 0x001F)
-
-    return dict_
 
 async def consumer():
     try:
@@ -55,10 +44,8 @@ async def decode_json():
         try:
             line = jsonpickle.decode(result)
         except json.JSONDecodeError:
-            #stri = "not json"
             print("not json: " + result)
         else:
-            #do processing here?
             return line
 
 async def process_frame():
@@ -66,12 +53,13 @@ async def process_frame():
     if frame:
         sprites = [sprite for sprite in frame['sprites'].values()]
         palettes = frame['palette']
-        masked = [mask_sprite_data(sprite) for sprite in sprites]
+        masked = [helper.mask_sprite_data(sprite) for sprite in sprites]
 
         for i, v in enumerate(masked):
             pal = v['pal_number']
             masked[i]['palette'] = palettes[pal]
             del masked[i]['pal_number']
+
         sprites = [Sprite.fromdict(dict_) for dict_ in masked]
         return (frame['frame_number'], sprites)
 
@@ -80,13 +68,27 @@ async def main(p, folder):
     while p.poll() is None:
         result = await process_frame()
         if result:
-            file_name = 'frame_' + str(result[0]) + ".txt"
+            file_name = 'frame_' + str(result[0]) + '.txt'
             with open(save_ + file_name, 'w') as f:
-                f.write(jsonpickle.encode(result[1]))
+                for res in result[1]:
+                    f.write(jsonpickle.encode(res) + "\n")
 
-def coroutine_temp(output_folder):
-    os.chdir("D:\\Desktop\\mame2\\")
-    cmd = ["mame", "vsav", "-autoboot_script", "scrape_sprite_pal_data.lua", "-window"]
+    remaining = Q.qsize()
+    print(str(remaining) + " frames left")
+
+    for i in range(remaining):
+        result = await process_frame()
+        if result:
+            print(str(Q.qsize()) + " left in the queue")
+            file_name = 'frame_' + str(result[0]) + '.txt'
+            with open(save_ + file_name, 'w') as f:
+                for res in result[1]:
+                    f.write(jsonpickle.encode(res) + "\n")
+
+if __name__ == "__main__":
+    coroutine_temp()
+    os.chdir(MAME_PATH)
+    cmd = ["mame64", "vsav", "-autoboot_script", LUA_SCRIPT, "-window", "-nomax"]
 
     p = subprocess.Popen(cmd, bufsize=1, universal_newlines=True, stdout=subprocess.PIPE)
 
@@ -94,10 +96,6 @@ def coroutine_temp(output_folder):
     t.start()
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(p, output_folder))
+    loop.run_until_complete(main(p, SPRITE_OUTPUT_FOLDER))
     loop.close()
-
-if __name__ == "__main__":
-    output_folder = "\\outputs\\processed_vsav_json_files\\"
-    #pp_folder = "\\outputs\\pretty_vsav_json_files\\"
-    coroutine_temp(output_folder)
+    
