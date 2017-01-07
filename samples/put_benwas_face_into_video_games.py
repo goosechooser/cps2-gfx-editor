c03d09752195afa3e83c39b7264513b297d9b2f0
Marshall <xtremebass2k@gmail.com>
2017-01-07 06:32:07 -0800

    Lots of updates/

--- samples/put_benwas_face_into_video_games.py
+++ samples/put_benwas_face_into_video_games.py
@@ -1,13 +1,19 @@
 #from struct import iter_unpack
+import os
+import sys
+import mmap
 from PIL import Image
-import jsonpickle
 from src import tile_printer, Sprite, Tile, image_handler, helper
 
-GFX_FILE = "inputs/refactor/vm3.13.15.17.19.final"
-TILE_FILE = "inputs/op_info_json.txt"
+#Change as necessary
+GFX_FILE = "inputs/refactor/vm3_combined"
+GFX_F = open(GFX_FILE, 'r+b')
+GFX_MM = mmap.mmap(GFX_F.fileno(), 0)
+FRAMES_FOLDER = "inputs/smol_json/"
+OUTPUT_FOLDER = "outputs/cosprites3/"
 
 def do_it(gfx_file, tile_file):
-    dicts = fromlua(tile_file)
+    dicts = helper.fromlua(tile_file)
     sprites = [Sprite.fromdict(dict_) for dict_ in dicts]
 
     with open(gfx_file, 'r') as f:
@@ -20,16 +26,21 @@ def do_it(gfx_file, tile_file):
 
     return sorted_sprites
 
-def fromlua(lua):
-    with open(lua, 'r') as f:
-        return [jsonpickle.decode(line) for line in f]
+def frames_to_png(gfx_file, frames_folder, output_folder):
+    for filename in os.listdir(frames_folder):
+
+        sprites = helper.fromlua(frames_folder + filename)
+
+        for i, sprite in enumerate(sprites):
+            tiles2d = tile_printer.make_tiles_mmap(GFX_MM, sprite.addrs2d())
+            sprites[i].tiles = helper.flatten_list(tiles2d)
+
+        put_sprites(sprites, output_folder + filename[:-4])
 
 def print_sprites(sprites, path):
-    print("printing sprites")
+    """Turns individual sprites into .png files"""
     for i, v in enumerate(sprites):
         v.topng(path + v.tiles[0].address)
-
-    print("done printing")
 
 def write_sprites(sprite, image):
     tiles = image_handler.to_tiles(image, sprite.addrs2d(), sprite.palette)
@@ -46,14 +57,17 @@ def stitch_sprites(sprites, output):
     image.save(output + ".png")
 
 def put_sprites(sprites, output):
-    canvas = Image.new('RGB', (1024, 1024))
+    """Takes a list of sprites and creates a .png image.
+    Sprites are placed in the image at the x,y location they would occupy on the cps2.
+    """
+    canvas = Image.new('RGB', (400, 400))
     for sprite in sprites:
         im = Image.fromarray(sprite.toarray(), 'RGB')
         canvas.paste(im, sprite.location)
     canvas.save(output + ".png")
 
 if __name__ == "__main__":
-    sprites = do_it(GFX_FILE, TILE_FILE)
+    frames_to_png(GFX_FILE, FRAMES_FOLDER, OUTPUT_FOLDER)
     #stitch_sprites(sprites, "outputs/temp")
     #print_sprites(sprites, "outputs/sprites/")
     #put_sprites(sprites,"outputs/put_test")
