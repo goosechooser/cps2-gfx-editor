1c5e54133684fe65b1d47bb162052cb3efc71b33
Marshall <xtremebass2k@gmail.com>
2017-01-07 06:19:34 -0800

    Changed imports

--- samples/file_handler_sample.py
+++ samples/file_handler_sample.py
@@ -1,12 +1,12 @@
-from gfx_file_handler import interleave_files, deinterleave_files
+from src import gfx_file_handler
 
 if __name__ == "__main__":
-    #GFX_DATA = interleave_files("inputs/gfx_original")
+    #GFX_DATA = gfx_file_handler.interleave_files("inputs/gfx_original")
     #for k, v in GFX_DATA.items():
     #    with open('outputs/refactor/roms/' + k, 'wb') as f:
     #        f.write(v)
 
-    GFX_DATA = deinterleave_files("inputs/roms")
+    GFX_DATA = gfx_file_handler.deinterleave_files("inputs/roms")
     for k, v in GFX_DATA.items():
         with open('outputs/roms/' + k + 'm', 'wb') as f:
             f.write(v)
